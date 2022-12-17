2ff4233a23b1ed0051cc0f9399e00c33357092e8
ammarraihn <ammarraihan00@gmail.com>
2022-12-17 18:57:11 +0700

    credentials

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,14 +2,14 @@ import "../style/Login.css";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Login = (props) => {
-    const [email, setEmail] = useState('');
+const Login = () => {
+    const [username, setUsername] = useState('');
     const [pass, setPass] = useState('');
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (email === "admin" && pass === "admin") {
+        if (username === "ammar" & pass === "191221" || username === "anien" & pass === "191221") {
             console.log("Login successful");
             navigate("/gallery");
             localStorage.setItem("user", "admin");
@@ -23,9 +23,9 @@ const Login = (props) => {
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
                 <div>username</div>
-                <input className="username" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="username" id="email" name="email" />
+                <input className="username" value={username} onChange={(e) => setUsername(e.target.value)} id="username" name="username" />
                 <div>password</div>
-                <input className="password" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <input className="password" value={pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password" />
                 <button className="login-btn" type="submit">Log In</button>
             </form>
         </div>
